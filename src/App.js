d029eb1e671def799dcc914889877ca108b76672
CrumrineCoder <crumrinecoding@gmail.com>
2019-02-09 18:25:58 -0500

    set parent index to 1!

--- src/App.js
+++ src/App.js
@@ -9,47 +9,58 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = [{
-      logo: "Images/votingAppLogo.png ",
-      color: "#A61C88",
-      title: "Polling",
-      shortDesc: "This React.js & Redux fullstack website lets users create, view, and vote on polls.",
-      backend: "JavaScript, Node.js, Express.js, MongoDB, Mongoose, jwt, Passport",
-      frontend: "React.js, Redux.js, rxJs, react-router, HTML5, CSS3, Bootstrap 4",
-      productPaper: "This website is an updated version of one I made last year. I taught myself Redux and react-router while developing my React.js fullstack skills. I am still adding new features.",
-      codeLink: "https://github.com/CrumrineCoder/Polling",
-      websiteLink: "https://crumrinepolling.herokuapp.com/#/",
-      prevWebsiteLink: "https://joinordie.glitch.me/",
-      caseStudyLink: "https://nicolascrumrine.herokuapp.com/#/posts/5c253ffad802b53cdcc17e00", 
-      video: "Videos/pollingReactEdited.mp4"
-  },{
-      logo: "Images/tmbgLogo.png ",
-      color: "#171E26",
-      title: "TMBG",
-      shortDesc: "This is a landing page redesign for the They Might Be Giants website.",
-      backend: "JavaScript, jQuery, Node.js, Express.js, MongoDB, Mongoose, OpenStreetMap API, LyricsAPI",
-      frontend: "HTML5, CSS3, Responsive, Angular, Sass",
-      productPaper: "I built this website as a Capstone project for everything I've learnt in the past year. The website allows users to anonymously comment on the newest album and to find the nearest tour. The website is also a practice in business marketing and design. Links and most buttons are placeholders.",
-      codeLink: "https://github.com/CrumrineCoder/TheyMightBeGiantsv3",
-      websiteLink: "https://tmbg.herokuapp.com/",
-      video: "Videos/tmbgEdited.mp4"
-  }, {
-      logo: "Images/cookiesWebsiteLogo.png ",
-      color: "#18788C",
-      picture: " Images/cookiesWebsite.png ",
-      title: "Penny's Cookies",
-      shortDesc: "This is a portfolio website for a local NYC cookie bakery and decorating business that I built for a client.",
-      backend: "JavaScript, PHP, jQuery",
-      frontend: "HTML5, CSS3, Responsive, Swiper, Intense Images, Mail Chimp",
-      productPaper: "I taught myself about developing a marketing strategy, writing a competitive analysis, scheduling, and writing user stories with a client. I also learned about working with another designer. I taught myself PHP to develop the email forms. I also taught myself a few other minor libraries and services.",
-      codeLink: "https://github.com/CrumrineCoder/PJRevised",
-      websiteLink: "http://pennyjanescookies.com/",
-      video: "Videos\\pjcEdited.mp4"
-  }];
+    this.state = {
+      projects: [{
+        logo: "Images/votingAppLogo.png ",
+        color: "#A61C88",
+        title: "Polling",
+        shortDesc: "This React.js & Redux fullstack website lets users create, view, and vote on polls.",
+        backend: "JavaScript, Node.js, Express.js, MongoDB, Mongoose, jwt, Passport",
+        frontend: "React.js, Redux.js, rxJs, react-router, HTML5, CSS3, Bootstrap 4",
+        productPaper: "This website is an updated version of one I made last year. I taught myself Redux and react-router while developing my React.js fullstack skills. I am still adding new features.",
+        codeLink: "https://github.com/CrumrineCoder/Polling",
+        websiteLink: "https://crumrinepolling.herokuapp.com/#/",
+        prevWebsiteLink: "https://joinordie.glitch.me/",
+        caseStudyLink: "https://nicolascrumrine.herokuapp.com/#/posts/5c253ffad802b53cdcc17e00",
+        video: "Videos/pollingReactEdited.mp4"
+      }, {
+        logo: "Images/tmbgLogo.png ",
+        color: "#171E26",
+        title: "TMBG",
+        shortDesc: "This is a landing page redesign for the They Might Be Giants website.",
+        backend: "JavaScript, jQuery, Node.js, Express.js, MongoDB, Mongoose, OpenStreetMap API, LyricsAPI",
+        frontend: "HTML5, CSS3, Responsive, Angular, Sass",
+        productPaper: "I built this website as a Capstone project for everything I've learnt in the past year. The website allows users to anonymously comment on the newest album and to find the nearest tour. The website is also a practice in business marketing and design. Links and most buttons are placeholders.",
+        codeLink: "https://github.com/CrumrineCoder/TheyMightBeGiantsv3",
+        websiteLink: "https://tmbg.herokuapp.com/",
+        video: "Videos/tmbgEdited.mp4"
+      }, {
+        logo: "Images/cookiesWebsiteLogo.png ",
+        color: "#18788C",
+        picture: " Images/cookiesWebsite.png ",
+        title: "Penny's Cookies",
+        shortDesc: "This is a portfolio website for a local NYC cookie bakery and decorating business that I built for a client.",
+        backend: "JavaScript, PHP, jQuery",
+        frontend: "HTML5, CSS3, Responsive, Swiper, Intense Images, Mail Chimp",
+        productPaper: "I taught myself about developing a marketing strategy, writing a competitive analysis, scheduling, and writing user stories with a client. I also learned about working with another designer. I taught myself PHP to develop the email forms. I also taught myself a few other minor libraries and services.",
+        codeLink: "https://github.com/CrumrineCoder/PJRevised",
+        websiteLink: "http://pennyjanescookies.com/",
+        video: "Videos\\pjcEdited.mp4"
+      }],
+      index: 0
+    };
     this.toggleOverlay = this.toggleOverlay.bind(this);
     this.pause = this.pause.bind(this);
     this.play = this.play.bind(this);
+   // this.updateProjectIndexHandler = this.updateProjectIndexHandler.bind(this)
   }
+
+  handleLanguage = (langValue) => {
+    console.log(langValue);
+    this.setState({index: langValue});
+    console.log(this.state);
+}
+
 
   toggleOverlay() {
     console.log("test");
@@ -73,7 +84,7 @@ class App extends Component {
               <p id="nic"> Nicolas Crumrine </p>
               <p id="nicJob">Front End Web Developer based in NYC
                   <a id="aboutMeButton" href="https://nicolascrumrine.herokuapp.com/#/about" rel="noopener noreferrer" target="_blank">About
-              Me
+            Me
                   </a>
               </p>
             </li>
@@ -94,8 +105,8 @@ class App extends Component {
             </li>
           </ul>
         </nav>
-        <Boxes projects = {this.state} />
-        <Project {...this.state[0]} />
+        <Boxes  onSelectLanguage={this.handleLanguage} projects={this.state.projects} />
+        <Project {...this.state.projects[this.state.index]} />
 
       </div>
     );
