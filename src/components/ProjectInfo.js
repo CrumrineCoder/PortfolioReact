e75a3faba3757727a67d27348d330dd110b15d2b
CrumrineCoder <crumrinecoding@gmail.com>
2019-05-30 19:50:22 -0400

    Animation change on state chang esuccessful

--- src/components/ProjectInfo.js
+++ src/components/ProjectInfo.js
@@ -1,9 +1,7 @@
 
-
-import React, { Component, useState, useCallback } from "react";
-import { Spring, Transition } from 'react-spring/renderprops';
-import { useTransition, animated, config } from 'react-spring'
-import Fade from 'react-reveal/Fade';
+import React, { Component } from "react";
+import { Spring } from 'react-spring/renderprops';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 class ProjectInfo extends Component {
     constructor(props) {
@@ -15,41 +13,14 @@ class ProjectInfo extends Component {
     }
 
     render() {
-
-        /*   const pages = [
-               ({ style }) => <animated.div style={{ ...style, background: 'lightpink' }}>A</animated.div>,
-               ({ style }) => <animated.div style={{ ...style, background: 'lightblue' }}>B</animated.div>,
-               ({ style }) => <animated.div style={{ ...style, background: 'lightgreen' }}>C</animated.div>,
-           ]
-   
-      
-               const [index, set] = useState(0)
-               const onClick = useCallback(() => set(state => (state + 1) % 3), [])
-               const transitions = useTransition(index, p => p, {
-                   from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
-                   enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
-                   leave: { opacity: 0, transform: 'translate3d(-50%,0,0)' },
-               })
-               return (
-                   <div className="simple-trans-main" onClick={onClick}>
-                       {transitions.map(({ item, props, key }) => {
-                           const Page = pages[item]
-                           return <Page key={key} style={props} />
-                       })}
-                   </div>
-               )
-           
-   
-           render(<App />, document.getElementById('root')) */
-        let projects = this.props.projects;
+        console.log(this.props);
         return (
-            <Fade right>
-                <div className="landingTitle">
-                    {this.props.selectedProjectIndex !== null
-                        ? this.props.projects[this.props.selectedProjectIndex].title
-                        : "Nicolas Crumrine"}
-                </div>
-            </Fade>
+            <ReactCSSTransitionGroup
+                transitionName="example"
+                transitionEnterTimeout={3000}
+                transitionLeaveTimeout={3000}>
+                <div key={this.props.selectedProjectIndex} className="landingTitleBody">Test</div>
+            </ReactCSSTransitionGroup>
         );
     }
 }
@@ -57,73 +28,77 @@ class ProjectInfo extends Component {
 export default ProjectInfo;
 
 /*
-  <div className="landingTitle">
-                                        {this.props.selectedProjectIndex !== null
-                                            ? this.props.projects[this.props.selectedProjectIndex].title
-                                            : "Nicolas Crumrine"}
+               <div key={this.props} className="landingTitleBody">
+                    <div className="landingTitle">
+                        {this.props.selectedProjectIndex !== null
+                            ? this.props.projects[this.props.selectedProjectIndex].title
+                            : "Nicolas Crumrine"}
+                    </div>
+                    {this.props.selectedProjectIndex !== null ? (
+                        <div>
+                            <div className="landingAdditionalInfo">
+                                <ul className="landingAdditionalInfoPills">
+                                    <div id="projectFrontEnd">
+                                        <li className="frontend">
+                                            {
+                                                this.props.projects[this.props.selectedProjectIndex]
+                                                    .frontend
+                                            }
+                                        </li>
                                     </div>
-                                    {this.props.selectedProjectIndex !== null ? (
-                                        <div>
-                                            <div className="landingAdditionalInfo">
-                                                <ul className="landingAdditionalInfoPills">
-                                                    <div id="projectFrontEnd">
-                                                        <li className="frontend">
-                                                            {
-                                                                this.props.projects[this.props.selectedProjectIndex]
-                                                                    .frontend
-                                                            }
-                                                        </li>
-                                                    </div>
-                                                    <div id="projectBackEnd">
-                                                        <li className="backend">
-                                                            {
-                                                                this.props.projects[this.props.selectedProjectIndex]
-                                                                    .backend
-                                                            }
-                                                        </li>
-                                                    </div>
-                                                </ul>
-                                                <p>
-                                                    {
-                                                        this.props.projects[this.props.selectedProjectIndex]
-                                                            .shortDesc
-                                                    }
-                                                </p>
-                                                <p>
-                                                    {
-                                                        this.props.projects[this.props.selectedProjectIndex]
-                                                            .productPaper
-                                                    }
-                                                </p>
-                                                <a
-                                                    onClick={this.userClicksPrevious}
-                                                    href={
-                                                        this.props.projects[this.props.selectedProjectIndex]
-                                                            .prevWebsiteLink
-                                                    }
-                                                    style={{
-                                                        display:
-                                                            this.props.projects[this.props.selectedProjectIndex]
-                                                                .prevWebsiteLink != undefined
-                                                                ? "block"
-                                                                : "none"
-                                                    }}
-                                                    className="externalLinks"
-                                                    rel="noopener noreferrer"
-                                                    target="_blank"
-                                                >
-                                                    <div id="prev-code-link">
-                                                        <i
-                                                            className="fas fa-external-link-alt fontIcon"
-                                                            rgb="(0,0,0)"
-                                                        >
-                                                        </i>
-                                                        See Previous Version of Website
-                                         </div>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    ) : (
-                                            <div className="landingSubTitle">Front End Web Developer</div>
-                                        )}
-                                        */
+                                    <div id="projectBackEnd">
+                                        <li className="backend">
+                                            {
+                                                this.props.projects[this.props.selectedProjectIndex]
+                                                    .backend
+                                            }
+                                        </li>
+                                    </div>
+                                </ul>
+                                <p>
+                                    {
+                                        this.props.projects[this.props.selectedProjectIndex]
+                                            .shortDesc
+                                    }
+                                </p>
+                                <p>
+                                    {
+                                        this.props.projects[this.props.selectedProjectIndex]
+                                            .productPaper
+                                    }
+                                </p>
+                                <a
+                                    onClick={this.userClicksPrevious}
+                                    href={
+                                        this.props.projects[this.props.selectedProjectIndex]
+                                            .prevWebsiteLink
+                                    }
+                                    style={{
+                                        display:
+                                            this.props.projects[this.props.selectedProjectIndex]
+                                                .prevWebsiteLink != undefined
+                                                ? "block"
+                                                : "none"
+                                    }}
+                                    className="externalLinks"
+                                    rel="noopener noreferrer"
+                                    target="_blank"
+                                >
+                                    <div id="prev-code-link">
+                                        <i
+                                            className="fas fa-external-link-alt fontIcon"
+                                            rgb="(0,0,0)"
+                                        >
+                                        </i>
+                                        See Previous Version of Website
+                                       </div>
+                                </a>
+                            </div>
+                        </div>
+                    ) : (
+                            <div className="landingSubTitle">Front End Web Developer</div>
+                        )}
+                </div>
+                )
+                }
+                */
