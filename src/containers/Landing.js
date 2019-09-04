4380519f9d816d2627c63d3b6f613aa6610abb27
CrumrineCoder <crumrinecoding@gmail.com>
2019-09-04 10:06:20 -0400

    Added bardic inspiration, zoomed in on profile pic

--- src/containers/Landing.js
+++ src/containers/Landing.js
@@ -26,6 +26,23 @@ class Landing extends Component {
       ],
       projects: [
         {
+          logo: "Images/bardicLogo.png ",
+          color: "#E9C46A",
+          projectName: "Bardic Inspiration",
+          shortDesc: "MERN Music Tagging site",
+          frontend:
+            "React.js, React Hooks, Redux.js, react-router, HTML5, CSS3",
+          backend: "JavaScript, AWS, MongoDB, Mongoose, Express.js, Node.js, JWT, Passport, OpenVPN",
+          productPaper:
+            "This website was meant to be a simple implementation of a tool, but grew into a study on React Hooks and Sagas, AWS and web security, Redux, and more.",
+          codeLink: "https://github.com/CrumrineCoder/Bard-Release",
+          websiteLink: "https://confectionswithcharacter.com/",
+          caseStudyLink:
+            "https://nicolascrumrine.com/#/posts/-Lntx2aWkC23w704-4z-",
+          video: "Videos/BardicInspiration.mp4",
+          class: "Bard"
+        },
+        {
           logo: "Images/raptureLogo.png ",
           color: "#218F7D",
           projectName: "Rapture Fashion",
@@ -58,7 +75,7 @@ class Landing extends Component {
           websiteLink: "https://polling-269dc.firebaseapp.com/#/",
           // prevWebsiteLink: "https://joinordie.glitch.me/",
           caseStudyLink:
-            "https://nicolascrumrine.herokuapp.com/#/posts/5c253ffad802b53cdcc17e00",
+            "https://nicolascrumrine.com/#/posts/-L_33SYjMlSMzQigh4QA",
           video: "Videos/Polling.mp4",
           class: "Poll"
         },
